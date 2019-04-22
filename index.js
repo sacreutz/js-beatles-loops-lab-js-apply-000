d6862634ae8073ff89598b0b861762f010483d8f
sacreutz <sacreutz@gmail.com>
2019-04-22 22:33:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  let newArray = []
+  
+  for (let i = 0; i < musicians.length; i++){
+    let string = musicians[i] + ' plays ' + instruments[i];
+    newArray.push(string)
+  }
+  return newArray; 
+}
